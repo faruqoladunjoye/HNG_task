2774c3fcdb48334f340b3e8ad4280d9a85ff665e
Faruq Oladunjoye <oladunjoyefaruq@yahoo.com>
2023-09-10 08:48:44 +0100

    first commit

---
+++ task.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+
+app.get("/task", (req, res) => {
+  const slackName = req.query.slack_name;
+  const track = req.query.track;
+
+  const currentDate = new Date();
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const currentDay = daysOfWeek[currentDate.getUTCDay()];
+  const currentUTCTime = currentDate.toISOString();
+
+  const response = {
+    slack_name: slackName,
+    current_day: currentDay,
+    utc_time: currentUTCTime,
+    track: track,
+    github_file_url: "https://github.com/Faruq",
+    github_repo_url: "https://github.com/Faruq/HNG",
+    status_code: 200,
+  };
+  res.json(response);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000.");
+});
